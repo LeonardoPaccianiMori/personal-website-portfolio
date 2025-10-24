8569f44d0afc4294c660177d64ddc13bb53b5695
LeonardoPaccianiMori <30660594+LeonardoPaccianiMori@users.noreply.github.com>
2025-10-24 16:41:07 +0000

    Deploying to gh-pages from @ LeonardoPaccianiMori/personal-website-portfolio@b3009832d16c3b23cf8a09fce02092b3f25f46e1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,13 +30,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/books/";
           },
-        },{id: "nav-cv",
-          title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/cv/";
-          },
         },{id: "post-building-an-llm-powered-insights-engine-that-doesn-39-t-hallucinate",
         
           title: "Building an LLM-Powered Insights Engine That Doesn&#39;t Hallucinate",
